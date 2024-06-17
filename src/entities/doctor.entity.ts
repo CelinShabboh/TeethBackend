8496ed1b7510457d741ec41ce729b68a919756cf
celin shabboh <programmingto2025@gmail.com>
2024-06-18 00:45:54 +0300

    tokens

--- src/entities/doctor.entity.ts
+++ src/entities/doctor.entity.ts
@@ -12,6 +12,7 @@ import { Content } from './same.entity';
 import { DoctorCondition } from './doctorCondition.entity';
 import { DoctorSession } from './doctorSession.entity';
 import { ResetToken } from './resetTokenSchema.entity';
+import { Tokens } from './tokens.entity';
 @Entity('doctors')
 export class Doctor extends Content {
   @Index({ unique: true })
@@ -36,8 +37,14 @@ export class Doctor extends Content {
   @OneToMany(() => DoctorSession, (session) => session.doctor, { eager: false })
   sessions: DoctorSession[];
   @OneToMany(() => ResetToken, (resetToken) => resetToken.doctor, {
-    eager: true,
+    eager: false,
     cascade: true,
   })
   resetTokens: ResetToken[];
+
+  @OneToMany(() => Tokens, (tokens) => tokens.doctor, {
+    eager: false,
+    cascade: true,
+  })
+  tokens: Tokens[];
 }
