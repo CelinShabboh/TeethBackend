d1832c98a066704181332ae25021b362ea61dbeb
CelinShabboh <157270940+CelinShabboh@users.noreply.github.com>
2024-06-17 19:41:13 +0300

    Update updateDto.ts

--- src/dto/updateDto.ts
+++ src/dto/updateDto.ts
@@ -1,10 +1,14 @@
 import { CreateDoctorDto } from 'src/dto/createDto';
 import { CreateUserDto } from 'src/dto/createDto';
-import { PartialType } from '@nestjs/mapped-types';
-export class UpdateDoctorDto extends PartialType(CreateDoctorDto) {
+import { OmitType } from '@nestjs/mapped-types';
+export class UpdateDoctorDto extends OmitType(CreateDoctorDto, [
+  'password',
+] as const) {
   photo?: string; //تخزن في صيغة base64 لترميز الصور
 }
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
+export class UpdateUserDto extends OmitType(CreateUserDto, [
+  'password',
+] as const) {
   photo?: string;
 }
